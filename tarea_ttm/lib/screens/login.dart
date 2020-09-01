560b01990a0bf4efdf0cf5dd7d11ddd31aeefb62
Pablo Barría <27pablo08@gmail.com>
2020-09-01 01:14:48 -0400

    Separar de widgets en login y agregar controllers

--- tarea_ttm/lib/screens/login.dart
+++ tarea_ttm/lib/screens/login.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tarea_ttm/screens/home.dart';
 
+final userNameController = TextEditingController();
+final passwordController = TextEditingController();
+
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,58 +15,73 @@ class LoginScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.tealAccent[300],
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'User',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                ),
-                textAlign: TextAlign.center,
-              ),
-              width: 220,
-              margin: EdgeInsets.all(10),
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                ),
-                textAlign: TextAlign.center,
-                obscureText: true,
-              ),
-              width: 220,
-              margin: EdgeInsets.all(5),
-            ),
-            SizedBox(
-              width: 220,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                child: Text('Login'),
-                color: Colors.lightBlue[600],
-                onPressed: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()),
-                  ),
-                },
-                textColor: Colors.white,
-              ),
-            )
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          _readUserName(),
+          _readPassword(),
+          _login(context),
+        ],
       ),
     );
   }
+}
+
+Widget _readUserName() {
+  return Container(
+    child: TextFormField(
+      controller: userNameController,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.account_box),
+        hintText: 'Username',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      textAlign: TextAlign.center,
+    ),
+    width: 220,
+    margin: EdgeInsets.all(10),
+  );
+}
+
+Widget _readPassword() {
+  return Container(
+    alignment: Alignment.center,
+    child: TextFormField(
+      controller: passwordController,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        prefixIcon: Icon(Icons.lock),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      textAlign: TextAlign.center,
+      obscureText: true,
+    ),
+    width: 220,
+    margin: EdgeInsets.all(5),
+  );
+}
+
+Widget _login(context) {
+  return SizedBox(
+    width: 220,
+    child: RaisedButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Text('Login'),
+      color: Colors.lightBlue[600],
+      onPressed: () => {
+        print(userNameController.text),
+        print(passwordController.text),
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+        ),
+      },
+      textColor: Colors.white,
+    ),
+  );
 }
